99e5aff52d94dbaa6097c452175e3a2cb7827c6d
Martin Callesen <martin.callesen@bec.dk>
2020-12-03 11:29:31 +0100

    Added todo's to producer and consumer

--- consumer/src/main/java/com/github/kafka/SimpleConsumerDemo.java
+++ consumer/src/main/java/com/github/kafka/SimpleConsumerDemo.java
@@ -3,5 +3,12 @@ package com.github.kafka;
 public class SimpleConsumerDemo {
     public static void main(String[] args) {
         System.out.println("Consumer is running");
+        //Todo Create consumer configuration for connection to local kafka instance
+
+        //Todo Create consumer
+
+        //Todo subscribe consumer to topics
+
+        //Todo Poll for new data and write key, val, partition and offset to console
     }
 }
--- producer/src/main/java/com/github/kafka/SimpleProducerDemo.java
+++ producer/src/main/java/com/github/kafka/SimpleProducerDemo.java
@@ -3,5 +3,10 @@ package com.github.kafka;
 public class SimpleProducerDemo {
     public static void main(String[] args) {
         System.out.println("Producer is running");
+        //Todo Create producer properties for connection to local kafka instance
+
+        //Todo Create the Producer
+
+        //Todo send data to topic
     }
 }
