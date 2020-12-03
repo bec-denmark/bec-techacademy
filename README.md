af8480c721fd0ad3f02f842c46e40f553ae0f858
Martin Callesen <martin.callesen@bec.dk>
2020-12-03 14:39:00 +0100

    Added scripts to readme and change to java 8

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ mvn compile exec:java
 Open a terminal and in directory ./consumer 
 mvn compile exec:java
 
+## Use full commands
+
+###Producer to topic with console producer
+./kafka-console-producer.sh --broker-list localhost:9092 --topic topic1
+
+###Consume from topic with console consumer
+./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic1
+
+###List Topics
+./kafka-topics.sh --bootstrap-server localhost:9092 --describe
+
+###See Consumer lag
+./kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --all-groups
+
--- producer/src/main/java/com/github/kafka/SimpleProducerSolution.java
+++ producer/src/main/java/com/github/kafka/SimpleProducerSolution.java
@@ -1,12 +1,32 @@
 package com.github.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class SimpleProducerSolution {
     public static void main(String[] args) {
         System.out.println("Producer is running");
         //Todo Create producer properties for connection to local kafka instance
+        String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         //Todo Create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         //Todo send data to topic
+        String topic = "topic1";
+        String value = "Hello world";
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, value);
+        producer.send(record);
+        producer.flush();
+        producer.close();
     }
 }
