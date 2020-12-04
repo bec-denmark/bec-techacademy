4ea5c6e6328af983cf3fd6e3475aad00617cc69e
Martin Callesen <martin.callesen@bec.dk>
2020-12-04 10:19:17 +0100

    removed unused imports

--- consumer/src/main/java/com/github/kafka/SimpleConsumerSolution.java
+++ consumer/src/main/java/com/github/kafka/SimpleConsumerSolution.java
@@ -5,7 +5,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
 import java.util.Properties;
