c877f4cae93892364414db089416bb49fa73a808
Martin Callesen <martin.callesen@bec.dk>
2020-12-04 14:18:42 +0100

    added callbackproducer exercise and consumer

---
+++ producer/src/main/java/com/github/kafka/CallbackProducerExercise.java
@@ -0,0 +1,16 @@
+package com.github.kafka;
+
+public class CallbackProducerExercise {
+    public static void main(String[] args) {
+        System.out.println("Producer with callback is running");
+        //Todo Create producer properties for connection to local kafka instance
+
+        //Todo Create the Producer
+
+        //Todo send data to topic
+
+        //Todo Print "Success" to console when a record was successfully send
+
+        //Todo Print "Failure" to console when a record was not successfully send
+    }
+}
